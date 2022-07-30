06aa84472c915107706fe3b0b2bb1bbeba44997b
phamquanganh <phamquanganh705@gmail.com>
2022-07-30 10:09:21 +0700

    add classpath and project

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,19 +3,33 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private double offset_x;
+    private double offset_y;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
 			Scene scene = new Scene(root);
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			scene.getStylesheets().add(getClass().getResource("/css/login.css").toExternalForm());
-			primaryStage.setTitle("Login");
+			
+			// Drag scene
+			scene.setOnMousePressed(event -> {
+	            offset_x = event.getSceneX();
+	            offset_y = event.getSceneY();
+	        });
+	        scene.setOnMouseDragged(event -> {
+	            primaryStage.setX(event.getScreenX() - offset_x);
+	            primaryStage.setY(event.getScreenY() - offset_y);
+	        });
+	        
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,15 +1,17 @@
 package controller;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
 
@@ -20,13 +22,16 @@ public class LoginController implements Initializable {
     private URL location;
 
     @FXML
+    private Label close;
+
+    @FXML
     private Button forgotPassword;
 
     @FXML
-    private JFXButton login;
+    private Label loginLabel;
 
     @FXML
-    private Label loginLabel;
+    private Label minimize;
 
     @FXML
     private PasswordField password;
@@ -36,6 +41,17 @@ public class LoginController implements Initializable {
 
     @FXML
     private TextField username;
+    
+    @FXML
+    private void handleClose(MouseEvent event) {
+    	System.exit(0);
+    }
+    
+    @FXML
+    private void handleMinimize(MouseEvent event) {
+    	Stage stage = (Stage) minimize.getScene().getWindow();
+        stage.setIconified(true);
+    }
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
