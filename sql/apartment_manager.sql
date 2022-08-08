21434b3c19754151a4803eefb3c3bc8743bf25d3
Trần Hồng Nhung <93045925+hongnhung250@users.noreply.github.com>
2022-08-08 15:02:02 +0700

    Add files via upload

--- sql/apartment_manager.sql
+++ sql/apartment_manager.sql
@@ -22,7 +22,7 @@ create table Chu_so_huu(
 );
 
 create table Phong(
-	Ma_phong varchar(20) not null,
+	Ma_phong int not null,
     Id_chu_so_huu varchar(20),
     So_nguoi int,
     Loai_phong varchar(20),
@@ -31,44 +31,45 @@ create table Phong(
     foreign key (Id_chu_so_huu) references Chu_so_huu(Id_chu_so_huu)
 );
 
-create table Dich_Vu(
+create table Don_Gia_Dich_Vu(
 	Ma_dich_vu int not null,
     Ten_dich_vu varchar(20),
-    Don_gia_bac_1 int,
-	Don_gia_bac_2 int,
-	Don_gia_bac_3 int,
-    Don_gia_bac_4 int,
+    Don_gia_bac_1 float,
+	Don_gia_bac_2 float,
+	Don_gia_bac_3 float,
+    Don_gia_bac_4 float,
+    Don_gia_bac_5 float,
+    Don_gia_bac_6 float,
     primary key (Ma_dich_vu)
 );
 
-create table Hoa_Don_Dich_Vu(
-	Ma_phong varchar(20) not null,
+create table Dich_Vu(
+	Ma_phong int not null,
     Ma_dich_vu int not null,
     So_cu int,
     So_moi int,
-    Tien_ve_sinh int,
-    No_cu float,
-    primary key (Ma_phong, Ma_dich_vu),
+    Thang date,
+    Da_dong bool,
+    primary key (Ma_phong, Ma_dich_vu, Thang),
     foreign key (Ma_phong) references Phong(Ma_phong),
-    foreign key (Ma_dich_vu) references Dich_Vu(Ma_dich_vu)
+    foreign key (Ma_dich_vu) references Don_Gia_Dich_Vu(Ma_dich_vu)
+);
+
+create table Don_Gia_Gui_Xe (
+	Loai_xe varchar(20),
+    Don_gia int,
+    primary key (Loai_xe)
 );
 
 create table Xe(
 	Ten_chu_xe varchar(40) not null,
+    Ma_phong int not null,
     Loai_xe varchar(20),
     Bien_so_xe varchar(20),
     Mau_sac varchar(20),
-    Don_gia_o_to int,
-    Don_gia_xe_may int,
-    Don_gia_xe_dap int,
-    primary key (Ten_chu_xe)
-);
-
-create table Hoa_Don_Xe(
-	Ten_chu_xe varchar(40) not null,
-	Ma_phong varchar(20) not null,
-    Tong_tien_xe int,
-    primary key (Ten_chu_xe, Ma_phong),
-    foreign key (Ten_chu_xe) references Xe(Ten_chu_xe),
-    foreign key (Ma_phong) references Phong(Ma_phong)
+    Thang date,
+    Da_dong bool,
+    primary key (Ten_chu_xe, Ma_phong, Loai_xe, Thang),
+    foreign key (Ma_phong) references Phong(Ma_phong),
+    foreign key (Loai_xe) references Don_Gia_Gui_Xe (Loai_xe)
 );
